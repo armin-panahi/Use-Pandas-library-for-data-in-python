f77b99f59929961196dc3f356ebd42d7af5c2166
𝗔𝗥𝗠𝗜𝗡 𝗣𝗔𝗡𝗔𝗛𝗜 <amirhosseinpanahi.business@gmail.com>
2025-10-22 19:07:42 +0330

    Add files via upload

---
+++ main_python.py
@@ -0,0 +1,20 @@
+# Main Python File
+
+# Libraries
+import pandas as pds
+
+
+# Create Data Frame
+dataFrame1 = pds.DataFrame({
+    "Username": ["arminp10"],
+    "=>": ["=>"],
+    "Password": ["armin1382"],
+})
+
+#  show
+print(dataFrame1)
+
+
+
+
+# Finish
